7b2bd4bbc906f5edd7c8a2dbb06c35d510bfcbd3
janrth <jan_rathfelder@yahoo.de>
2023-09-17 20:29:42 +0200

    change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="olscheck",
-    version="0.1.5",
+    version="0.1.6",
     author="Jan Rathfelder",
     author_email="jan_rathfelder@yahoo.de",
     description="A library to check OLS assumptions.",
